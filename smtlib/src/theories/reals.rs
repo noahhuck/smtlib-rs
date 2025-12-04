e8e453c92075a368ca9c3dedadf2792cbe57ec52
Noah Huck <noahhuck@cs.washington.edu>
2025-12-03 23:39:14 -0800

    change div to /

--- smtlib/src/theories/reals.rs
+++ smtlib/src/theories/reals.rs
@@ -140,4 +140,4 @@ impl std::ops::Neg for Real<'_> {
 impl_op!(Real<'st>, f64, Add, add, "+", AddAssign, add_assign, +);
 impl_op!(Real<'st>, f64, Sub, sub, "-", SubAssign, sub_assign, -);
 impl_op!(Real<'st>, f64, Mul, mul, "*", MulAssign, mul_assign, *);
-impl_op!(Real<'st>, f64, Div, div, "div", DivAssign, div_assign, /);
+impl_op!(Real<'st>, f64, Div, div, "/", DivAssign, div_assign, /);
